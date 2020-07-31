8c9791dda64473551d686d3158d04816ad882b77
lukasmyth96 <luka@nate.tech>
2020-07-31 15:31:19 +0100

    added end of quiz message using ternary statement

--- lib/main.dart
+++ lib/main.dart
@@ -14,44 +14,54 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _quizUnfinished = true;
+
+  static const questions = [
+    {
+      'questionText': 'what\'s your favorite colour?',
+      'answers': ['Black', 'Red', 'Green', 'White']
+    },
+    {
+      'questionText': 'what\'s your favorite animal?',
+      'answers': ['Dog', 'Cat', 'Rabbit', 'Snake']
+    },
+    {
+      'questionText': 'who\'s your favorite instructor?',
+      'answers': ['Bob', 'Bill', 'Max']
+    },
+  ];
 
   void _onAnswerClick() {
     setState(() {
       _questionIndex++;
     });
-    print('Answer Chosen!');
+    if (_questionIndex >= questions.length) {
+      _quizUnfinished = false;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'what\'s your favorite colour?',
-        'answers': ['Black', 'Red', 'Green', 'White']
-      },
-      {
-        'questionText': 'what\'s your favorite animal?',
-        'answers': ['Dog', 'Cat', 'Rabbit', 'Snake']
-      },
-      {
-        'questionText': 'who\'s your favorite instructor?',
-        'answers': ['Bob', 'Bill', 'Max']
-      },
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz App'),
         ),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]['questionText']),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answerText) => Answer(answerText, _onAnswerClick))
-                .toList(),
-          ],
-        ),
+        body: _quizUnfinished
+            ? Column(
+                children: [
+                  Question(questions[_questionIndex]['questionText']),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answerText) => Answer(answerText, _onAnswerClick))
+                      .toList(),
+                ],
+              )
+            : Center(
+                child: Text(
+                  'Thanks for completing the Quiz!',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
       ),
     );
   }
